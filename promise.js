9ebbcf61c00c7b624145c0756292cf88c7b568cd
DenyAnwar <denyanwardev@gmail.com>
2022-06-16 18:44:24 +0800

    Done

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (responseViewer) => {
+  try {
+    const [responseTheaterIXX, responseTheaterVGC] = await Promise.all([promiseTheaterIXX(), promiseTheaterVGC()])
+    
+    const result = [...responseTheaterIXX, ...responseTheaterVGC]
+
+    // filter result
+    return result?.filter(item => item.hasil === responseViewer)?.length || 0
+  } catch (error) {
+    console.log("there was an error: " + error);
+  }
+};
 
 module.exports = {
   promiseOutput,
